851b3f37dc0dbbbd98c16a698e2d7e703f075a04
ivan <ivan.morandi94@gmail.com>
2017-10-11 00:55:28 +0200

    Added chart of convergence

--- Practical 3/Exercise 2 - PageRank/exercise13.cpp
+++ Practical 3/Exercise 2 - PageRank/exercise13.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstdlib>
 #include <fstream>
 #include <ctime>
 #include <vector>
@@ -6,32 +7,65 @@
 
 using namespace std;
 
-double* page_rank(Graph* graph, double alpha, int iterations);
+void save_matrix(char* filename, Graph* graph, double** matrix, long* matrix_nodes, int nrows, int ncols);
+void print_matrix(Graph* graph, double** matrix, long* matrix_nodes, int nrows, int ncols);
+
+void print_array(double *P, int length);
+
+double* page_rank(Graph* graph, double alpha, int iterations, long* matrix_nodes, double** matrix_convergence, int matrix_length);
 void write_file(char* name,double* P,long length );
-long find_max(double* P, long length);
+void find_max_min(Graph *graph, double* P, long length, long &max_index, long &min_index);
 
 //TODO: CHANGE GRAPH STRUCTURE TO HAVE LINK IN AND OUT
 
 int main(int argc, char** argv) {
     Graph* graph;
+    long iteration = 10;
+    double alpha = 0.2;
 
     cout << "NSD Course:" << endl << endl;
     long start = time(NULL);
 
     cout << "Loading the graph into the data structure"<<endl;
+
+    if(argc > 3)
+        iteration = atoi(argv[3]);
+    if(argc > 2)
+        alpha = atof(argv[2]);
+
     if(argc > 1)
         graph = graph_load_file(argv[1]);
     else
         graph = graph_load_file("graph3.txt");
 
+
     //graph_print(graph);
     cout << "Done, required " << time(NULL) - start << " seconds" <<endl;
 
+    cout<<"Starting page rank with alpha="<<alpha<<" and #iteration="<<iteration<<endl;
 
-    cout<<"Starting page rank"<<endl;
-    double* P = page_rank(graph, 0.2, 10);
+    int number_nodes = 12;
 
-    cout << "Node with highest rank: "<<find_max(P, graph->number_nodes) + graph->offset << endl;
+    long matrix_nodes[] = {907722, 2421026, 4778009, 7196384, 10329063, 305, 6104545, 2031512, 6622645, 8285726, 3434750, 632};
+
+    double** matrix_convergence = new double*[number_nodes];
+
+    for (int i = 0; i < number_nodes; ++i) {
+        matrix_convergence[i] = new double[iteration];
+        matrix_nodes[i] -= graph->offset;
+    }
+
+    double* P = page_rank(graph, alpha, iteration, matrix_nodes, matrix_convergence, number_nodes);
+
+    save_matrix("output.dat", graph, matrix_convergence, matrix_nodes, number_nodes, iteration);
+
+    long max_index = 0;
+    long min_index = 0;
+
+    find_max_min(graph, P, graph->number_nodes, max_index, min_index);
+
+    cout << "Node with highest rank: "<<max_index + graph->offset << " with " << P[max_index] << endl;
+    cout << "Node with lowest rank: "<<min_index + graph->offset << " with " << P[min_index] <<endl;
 
     //cout<<"Writing on the file"<<endl;
     //write_file("output.txt", P, graph->number_nodes);
@@ -46,7 +80,7 @@ int main(int argc, char** argv) {
     return 0;
 }
 
-double* page_rank(Graph* graph, double alpha, int iterations){
+double* page_rank(Graph* graph, double alpha, int iterations, long* matrix_nodes, double** matrix_convergence, int matrix_length){
     double* P = new double[graph->number_nodes];
 
     double probability_come_from_neighbour = 0;
@@ -77,6 +111,10 @@ double* page_rank(Graph* graph, double alpha, int iterations){
             //cout << "P["<<i+graph->offset<<"] = (1 - " << alpha << " ) * " << probability_come_from_neighbour << " + " << alpha << " * " << probability_of_teletrasport << endl;
             P[i] = (1.0-alpha) * probability_come_from_neighbour + alpha * probability_of_teletrasport;
         }
+
+        for (int l = 0; l < matrix_length; ++l) {
+            matrix_convergence[l][k] = P[matrix_nodes[l]];
+        }
     }
     return P;
 }
@@ -92,16 +130,51 @@ void write_file(char* name,double* P,long length ){
     output.close();
 }
 
-long find_max(double* P, long length){
-    double max = P[0];
-    long max_index = 0;
+void find_max_min(Graph* graph, double* P, long length, long &max_index, long &min_index){
+    max_index = 0;
+    min_index = -1;
 
     for (int i = 0; i < length; ++i) {
-        if(max < P[i]) {
-            max = P[i];
+        if(min_index < 0 && ( graph->graph_degree_out[i] > 0 || graph->graph_degree_in[i] > 0))
+            min_index = i;
+        else{
+            if(min_index >= 0 && P[min_index] > P[i] && ( graph->graph_degree_out[i] > 0 || graph->graph_degree_in[i] > 0))
+                min_index = i;
+        }
+        if(P[max_index] < P[i]) {
             max_index = i;
         }
     }
+}
 
-    return max_index;
+void print_matrix(Graph* graph, double** matrix, long* matrix_nodes, int nrows, int ncols){
+    for (int i = 0; i < nrows; ++i) {
+        cout << matrix_nodes[i] + graph->offset <<": ";
+        for (int j = 0; j < ncols; ++j) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+void save_matrix(char* filename, Graph* graph, double** matrix, long* matrix_nodes, int nrows, int ncols){
+    ofstream output;
+    output.open(filename, ios::out);
+
+    for (int i = 0; i < ncols; ++i) {
+        //output << matrix_nodes[i] + graph->offset <<": ";
+        output << i+1 << " ";
+        for (int j = 0; j < nrows; ++j) {
+            output << matrix[j][i] << " ";
+        }
+        output << endl;
+    }
+
+    output.close();
+}
+
+void write_file_corr1(char* filename, Graph* graph, double* P){
+    for (int i = 0; i < graph->number_nodes; ++i) {
+
+    }
 }
